9eb9ec1f23df275d12b264399ad4967af8b7a9a0
Ehara-BW <s.ehara.jeudi11@gmail.com>
2025-11-23 16:43:33 +0900

    add:DB一覧の取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+
+        $todo = new Todo();
+        $todos = $todo->all();
+
+        return view('todo.index');
+
         return view('todo.index');
     }
 }
